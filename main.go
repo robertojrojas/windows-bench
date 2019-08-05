9330c8fd2610e0099da788abcda708512f537a78
Roberto J Rojas <robertojrojas@gmail.com>
2019-08-05 09:51:37 -0400

    adds new output info

--- main.go
+++ main.go
@@ -21,8 +21,9 @@ func main() {
 	if len(os.Args) > 3 {
 		password = os.Args[2]
 	}
+	host, _ := os.Hostname()
 
-	client, err := setupClient(password)
+	client, err := setupClient(host, password)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -30,13 +31,13 @@ func main() {
 	addPasswordPolicyQuery := fmt.Sprintf("Get-ADDefaultDomainPasswordPolicy -Identity %s | Select -ExpandProperty PasswordHistoryCount", domain)
 	cmd := fmt.Sprintf(powershellStr, addPasswordPolicyQuery)
 	var buffer bytes.Buffer
-	fmt.Printf("Running %q\n", cmd)
-	client.Run(cmd, &buffer, os.Stderr)
-	fmt.Printf("PasswordHistoryCount on %s\n", buffer.String())
+	var errBuffer bytes.Buffer
+	fmt.Printf("Running host: %s %q\n", host, cmd)
+	client.Run(cmd, &buffer, &errBuffer)
+	fmt.Printf("PasswordHistoryCount on %s err: %q\n", buffer.String(), errBuffer.String())
 }
 
-func setupClient(password string) (*winrm.Client, error) {
-	host, _ := os.Hostname()
+func setupClient(host, password string) (*winrm.Client, error) {
 	endpoint := winrm.NewEndpoint(host, 5985, false, false, nil, nil, nil, 0)
 	client, err := winrm.NewClient(endpoint, "Administrator", password)
 	return client, err

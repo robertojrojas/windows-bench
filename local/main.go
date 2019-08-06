f5d8c8639be4e9f80e6a00ab05567756caf923da
roberto rojas <robertojrojas@gmail.com>
2019-08-06 10:15:27 -0400

    adds new PS execution implementation

---
+++ local/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	ps "github.com/robertojrojas/go-powershell"
+	"github.com/robertojrojas/go-powershell/backend"
+)
+
+func main() {
+	// choose a backend
+	back := &backend.Local{}
+
+	// start a local powershell process
+	shell, err := ps.New(back)
+	if err != nil {
+		panic(err)
+	}
+	defer shell.Exit()
+
+	domain := "win-bench.pri"
+	pshell := fmt.Sprintf("Get-ADDefaultDomainPasswordPolicy -Identity %s | Select -ExpandProperty PasswordHistoryCount", domain)
+
+	if len(os.Args) > 2 {
+		pshell = os.Args[1]
+	}
+	stdout, stderr, err := shell.Execute(pshell)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(stdout, stderr)
+}
